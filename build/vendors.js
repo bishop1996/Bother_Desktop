80b82291c930e3a4c63587e63db6ea32c31e290b
bishop1996 <53303034+bishop1996@users.noreply.github.com>
2020-01-21 15:53:39 +0000

    blacker version

--- build/vendors.js
+++ build/vendors.js
@@ -47132,6 +47132,31 @@ eval("module.exports = __WEBPACK_EXTERNAL_MODULE_framer__;//# sourceURL=[module]
 });
 
 
+/***/ }),
+
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-macbook-pro/build/index.js":
+/*!****************************************************************************************************************************************************************************************************************************!*\
+  !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-macbook-pro/build/index.js ***!
+  \****************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 3820;
+        this.imageHeight = 2320;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
 /***/ })
 
 }]);
